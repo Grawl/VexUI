f88576ea7af5b0a0a209fded75d7ec301a25ba29
agora <agora@macbook.local>
2017-01-29 00:01:53 +0100

    Vexflow and VexUI added

--- build/index.js
+++ build/index.js
@@ -1,10 +1,37 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var name = function name(num, str) {
-  return str + num + "1";
-};
 
-exports.default = name;
+var _UI = require('./src/UI');
+
+var _Util = require('./src/Util');
+
+var _VexflowExtension = require('./src/VexflowExtension');
+
+var _Toolbar = require('./src/Toolbar');
+
+var _TipRenderer = require('./src/TipRenderer');
+
+var _Player = require('./src/Player');
+
+var _NoteMenuButtonRenderer = require('./src/NoteMenuButtonRenderer');
+
+var _NoteMenu = require('./src/NoteMenu');
+
+var _NoteMap = require('./src/NoteMap');
+
+var _MouseListener = require('./src/MouseListener');
+
+var _KeyboardListener = require('./src/KeyboardListener');
+
+var _Handler = require('./src/Handler');
+
+var _Constants = require('./src/Constants');
+
+var _Button = require('./src/Button');
+
+_UI.UI.Util = _Util.Util;
+
+exports.default = _UI.UI;
---
+++ build/src/UI.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+ * Vex UI - Andre Bakker <andre_bakker@yahoo.com.br>
+ * 		  - Allan Gir�o <allan.girao@gmail.com>
+ */
+
+/**
+ * New namespace.
+ */
+var UI = function UI() {};
