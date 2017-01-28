60c7ae5a57ef506e3a2e2cf26749c6fa2b12b917
agora <agora@macbook.local>
2017-01-28 22:35:21 +0100

    Initial commit

---
+++ build/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var name = function name(num, str) {
+  return str + num + "1";
+};
+
+exports.default = name;
---
+++ tests/index.js
@@ -0,0 +1,8 @@
+import test from 'tape';
+import name from '../build/index.js'
+
+test('test results', (t)=>{
+    t.same(1,1);
+
+    t.end();
+});
